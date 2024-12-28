3112540358e579ff0779a1b1a71862c0aeded1c5
4dydd <haidiraditya16@gmail.com>
2024-12-28 22:48:31 +0800

    error terbesar sepanjang guweh ngoding, error livewire nya

--- app/Livewire/Membership.php
+++ app/Livewire/Membership.php
@@ -12,6 +12,10 @@ class Membership extends Component
     public $hobi;
 
     public $deletedId = [];
+    public $editedId;
+    public $editedData;
+
+
 
     public $datas;
 
@@ -26,6 +30,34 @@ class Membership extends Component
         Member::create($validatedData);
 
         return redirect('/');
+    }
+
+    public function update()
+    {
+        $validatedData = $this->validate([
+            'nama' => 'required|string',
+            'kekayaan' => 'required|integer',
+            'hobi' => 'required|string'
+        ]);
+
+        Member::where('id', $this->editedId)->update($validatedData);
+        return redirect('/');
+    }
+
+    public function closeEditModal()
+    {
+        $this->editedId = null;
+        $this->editedData = null;
+    }
+
+    public function editDialog($id)
+    {
+        $this->editedId = $id;
+        $this->editedData = Member::where('id', $id)->first();
+
+        $this->nama = $this->editedData->nama;
+        $this->kekayaan = $this->editedData->kekayaan;
+        $this->hobi = $this->editedData->hobi;
     }
 
     public function delete($id)
--- resources/views/livewire/membership.blade.php
+++ resources/views/livewire/membership.blade.php
@@ -1,16 +1,57 @@
 <div>
 
+    {{-- ALERT GUWEH --}}
+    @if ($errors->any())
+        <div id="alert-2"
+            class="alert-gagal fixed transcenter animate-slideku flex p-4 mb-4 text-sm text-red-800 rounded-lg w-[30%] bg-red-50 dark:bg-gray-800 dark:text-red-400"
+            role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Danger</span>
+            <div>
+                <span class="font-medium">"ANUNYA" Gagal :</span>
+                <ul class="mt-1.5 list-disc list-inside">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+
+            {{-- tombol close --}}
+            <button type="button"
+                class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+                data-dismiss-target="#alert-2" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+            </button>
+            {{-- tombol close --}}
+        </div>
+    @endif
+    {{-- ALERT GUWEH --}}
+
+
+
     <div
         class="relative m-auto w-[60%] overflow-hidden overflow-x-auto rounded shadow-md sm:rounded-lg flex justify-center items-center">
-        <!-- Modal toggle -->
-        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
+
+        <!-- Create Modal toggle -->
+        <button data-modal-target="create-modal" data-modal-toggle="create-modal"
             class="absolute block top-0 right-0 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             type="button">
             Tambah Member
         </button>
+        <!-- Create Modal toggle -->
 
-        <!-- Main modal -->
-        <div id="crud-modal" tabindex="-1" aria-hidden="true"
+
+        <!-- Create modal -->
+        <div id="create-modal" tabindex="-1" aria-hidden="true"
             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div class="relative w-full max-w-md max-h-full p-4">
                 <!-- Modal content -->
@@ -22,7 +63,7 @@
                         </h3>
                         <button type="button"
                             class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-toggle="crud-modal">
+                            data-modal-toggle="create-modal">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -38,7 +79,7 @@
                             <div class="col-span-2">
                                 <label for="name"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                                <input type="text" wire:model='nama' name="nama" id="nama"
+                                <input type="text" wire:model='nama' id="nama"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="Masukkan nama" required="">
                             </div>
@@ -46,7 +87,7 @@
                             <div class="col-span-2 ">
                                 <label for="kekayaan"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kekayaan</label>
-                                <input type="number" wire:model='kekayaan' name="kekayaan" id="kekayaan"
+                                <input type="number" wire:model='kekayaan' id="kekayaan"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="$2999" required="">
                             </div>
@@ -55,7 +96,7 @@
                                 <label for="Hobi"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     Hobi</label>
-                                <textarea id="Hobi" wire:model='hobi' name="hobi" rows="4"
+                                <textarea id="Hobi" wire:model='hobi' rows="4"
                                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Tambahkan hobi disini..."></textarea>
                             </div>
@@ -75,6 +116,80 @@
                 </div>
             </div>
         </div>
+        <!-- Create modal -->
+
+
+
+        <!-- Update modal -->
+        @if ($editedData)
+            <div id="update-modal" tabindex="-1" aria-hidden="true"
+                class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative w-full max-w-md max-h-full p-4">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div
+                            class="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
+                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                Perbarui Member
+                            </h3>
+                            <button type="button" wire:click='closeEditModal'
+                                class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-toggle="update-modal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <form class="p-4 md:p-5" novalidate>
+                            <div class="grid grid-cols-2 gap-4 mb-4">
+
+                                <div class="col-span-2">
+                                    <label for="name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                                    <input type="text" wire:model='nama' name="nama" id="nama"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Masukkan nama" required="">
+                                </div>
+
+                                <div class="col-span-2 ">
+                                    <label for="kekayaan"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kekayaan</label>
+                                    <input type="number" wire:model='kekayaan' name="kekayaan" id="kekayaan"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="" required="">
+                                </div>
+
+                                <div class="col-span-2">
+                                    <label for="Hobi"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Hobi</label>
+                                    <textarea id="Hobi" wire:model='hobi' name="hobi" rows="4"
+                                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Tambahkan hobi disini..."></textarea>
+                                </div>
+
+                            </div>
+                            <button wire:click='update()' type="submit"
+                                class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="w-5 h-5 me-1 -ms-1" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                Update Member
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <!-- Update modal -->
 
         {{-- ALERT GUWEH --}}
         {{-- <div id="popup-modal" tabindex="1" data-modal-backdrop="static"
@@ -174,7 +289,7 @@
                                 {{ $value->kekayaan }}
                             </td>
                             <td class="px-6 py-4 text-right">
-                                <a href="#"
+                                <a href="#" wire:click='editDialog({{ $value->id }})'
                                     class="mx-1 font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                                 <a href="#" wire:click='delete({{ $value->id }})'
                                     class="mx-1 font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
